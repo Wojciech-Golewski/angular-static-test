8c9c6be676d435fa8d6c1959ae9946b5d4ca35d9
Wojtek <golewski.wojciech@outlook.com>
2019-11-18 16:56:43 +0100

    moved all material related imports to material module and added code for header

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,14 +5,8 @@ import { FooterComponent } from './components/footer/footer.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { MainComponent } from './components/main/main.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { LayoutModule } from '@angular/cdk/layout';
 import { TreeComponent } from './components/tree/tree.component';
-import { MatTreeModule } from '@angular/material/tree';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -27,13 +21,7 @@ import { MatTreeModule } from '@angular/material/tree';
   ],
   imports: [
     CommonModule,
-    MatGridListModule,
-    MatCardModule,
-    MatMenuModule,
-    MatIconModule,
-    MatButtonModule,
-    LayoutModule,
-    MatTreeModule
+    SharedModule,
   ],
   exports: [
     HeaderComponent,
--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,11 +1,35 @@
 import { NgModule } from '@angular/core';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+
 @NgModule({
   imports: [
-
+    MatToolbarModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule,
+    MatTreeModule
   ],
   exports: [
-
+    MatToolbarModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule,
+    MatTreeModule
   ]
 })
 export class MaterialModule { }
