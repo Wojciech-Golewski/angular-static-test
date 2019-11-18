27dee0a5b79caff343794f5f986e16afdedc504a
Wojtek <golewski.wojciech@outlook.com>
2019-11-18 17:32:45 +0100

    additional route for tree component from material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './core/components/main/main.component';
 import { NotFoundComponent } from './core/components/not-found/not-found.component';
+import { TreeComponent } from './core/components/tree/tree.component';
 
 
 const routes: Routes = [
+  {
+    path: 'material-file-tree',
+    component: TreeComponent
+  },
   {
     path: 'shop',
     loadChildren: () => import('./shop/shop.module')
