878738f8996e24fb7675d471b7b1d04e14b91277
Wojtek <golewski.wojciech@outlook.com>
2019-11-18 19:38:29 +0100

    table generated for material default

---
+++ src/app/shop/components/mat-default-table/mat-default-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mat-default-table',
+  templateUrl: './mat-default-table.component.html',
+  styleUrls: ['./mat-default-table.component.scss']
+})
+export class MatDefaultTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { ShopRoutingModule } from './shop-routing.module';
 import { ShopMainComponent } from './components/shop-main/shop-main.component';
 import { SharedModule } from '../shared/shared.module';
+import { MatDefaultTableComponent } from './components/mat-default-table/mat-default-table.component';
 
 
 @NgModule({
   declarations: [
-    ShopMainComponent
+    ShopMainComponent,
+    MatDefaultTableComponent
   ],
   imports: [
     CommonModule,
