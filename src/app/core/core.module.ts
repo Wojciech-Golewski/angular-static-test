b147403a3a063a2a689ab173ce5e9b74f60618d1
Wojtek <golewski.wojciech@outlook.com>
2019-11-18 16:09:32 +0100

    header, footer, and not-found component generated

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, FooterComponent, NotFoundComponent],
   imports: [
     CommonModule
   ]
