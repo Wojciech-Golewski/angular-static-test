585dd9e18d45b867e8b828d6334954d89d391c03
Wojtek <golewski.wojciech@outlook.com>
2019-11-18 16:11:04 +0100

    introduced main page and connected core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -7,9 +7,18 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, NotFoundComponent],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    NotFoundComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    NotFoundComponent
   ]
 })
 export class CoreModule { }
