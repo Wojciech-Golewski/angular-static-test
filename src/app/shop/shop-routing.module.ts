d00ccef234971f0af5cad3d5b29c5c5e27b0736d
Wojtek <golewski.wojciech@outlook.com>
2019-11-19 20:28:29 +0100

    connected table with its own separate component and renamed

--- src/app/shop/shop-routing.module.ts
+++ src/app/shop/shop-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { ShopMainComponent } from './components/shop-main/shop-main.component';
+import { TableMainComponent } from './components/table-main/table-main.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: ShopMainComponent
+    children: [
+      {
+        path: 'table',
+        component: TableMainComponent
+      }
+    ]
   }
 ];
 
